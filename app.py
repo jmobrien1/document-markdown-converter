2475af861501ffb0645fc0a5681d75a5c64d47ee
jmobrien1 <obrienmike@gmail.com>
2025-07-15 13:48:17 -0400

    Force redeploy with timestamp

--- app.py
+++ app.py
@@ -387,4 +387,4 @@ def too_large(e):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)# Deploy timestamp: Tue Jul 15 13:48:09 EDT 2025
