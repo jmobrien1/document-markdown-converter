c015a147bc0cc01220f78a234c9e47159acc31db
jmobrien1 <obrienmike@gmail.com>
2025-07-17 13:02:53 -0400

    fix: Use flask db upgrade for migrations

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# This command will run the db.create_all() for you on every deploy.
-# It's safe because create_all() doesn't re-create existing tables.
-flask create-db
+# Apply database migrations
+flask db upgrade
