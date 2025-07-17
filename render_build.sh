daffab1159d195ff745110147eec897c8c1ab05c
jmobrien1 <obrienmike@gmail.com>
2025-07-17 14:56:17 -0400

    build: Add debug step to list secret files

--- render_build.sh
+++ render_build.sh
@@ -2,5 +2,10 @@
 # exit on error
 set -o errexit
 
+# DEBUG: List the contents of the secrets directory
+echo "--- Listing contents of /etc/secrets/ ---"
+ls -la /etc/secrets/
+echo "----------------------------------------"
+
 pip install --upgrade pip
 pip install -r requirements.txt
